67bf74a200dcd467275a049a692dd2adadb54621
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-10-03 23:32:43 +0530

    Ex-04 done with the First Triangle Quiz
    in this We have done Our EX-04

--- App.js
+++ App.js
@@ -1,3 +1,30 @@
 const inputs=document.querySelectorAll(".all-angleinputs")
 
+const btndiv=document.querySelector("#submitbtn");
 
+const output=document.querySelector("#outputdiv");
+
+
+
+// to calculate the sum of all angle of triangle is 180
+
+function calculatesumofallangle(angle1,angle2,angle3){
+    const sum=angle1+angle2+angle3;
+    return sum;
+
+}
+function isTriangle(){
+    const angle=calculatesumofallangle(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+
+    if(angle===180){
+        output.innerText="Triangle Form AN ANGLE";
+
+    }
+    else{
+        output.innerText="OPPS Triangle Does Not form An Angle";
+        
+    }
+
+}
+
+btndiv.addEventListener("click",isTriangle)
